836556ecc957cb81c36c9160d9319a7d4c473e05
Stalyn Noboa <sonoboa@hotmail.com>
2025-05-20 19:35:15 -0400

    modified homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,6 @@
 export default function Home() {
   return (
     <>
-      <h1>Final Exam 343</h1>
-      <h2>Qureshi, Anique</h2>
-      <h3>Spring 2025</h3>
-    
       <h1 className="text-3xl font-bold text-gray-800 mb-2">Final Exam 343</h1>
       <h2 className="text-2xl font-semibold text-gray-700 mb-1">Qureshi, Anique</h2>
       <h3 className="text-xl text-gray-600">Spring 2025</h3>   
