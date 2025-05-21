30754ee0994fd4004fbf3aa081b33bc1d425119f
Stalyn Noboa <sonoboa@hotmail.com>
2025-05-20 21:51:51 -0400

    added faq page

--- src/app/components/menubar.jsx
+++ src/app/components/menubar.jsx
@@ -1,11 +1,20 @@
+import Link from 'next/link'
+
 export default function Menubar() {
-  return (
-    <>
-      <ul>
-        <li>Home</li>
-        <li>Midterm Part I</li>
-        <li>Midterm Part II</li>
-      </ul>
-          </>
-  )
-}
+    return (
+      <>
+        <ul>
+          
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <a href='https://expense-new-five.vercel.app/' target='_blank'>Midterm Part I - Expense</a>
+          </li>
+          <li>
+            <a href='https://website-exam-part-2-smoky.vercel.app/' target='_blank'>Midterm Part II Site</a>
+          </li>
+        </ul>
+      </>
+    )
+  }
---
+++ src/app/faq/page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-gray-800 mb-2">FAQ</h1>
+         
+    </>
+  )
+}
